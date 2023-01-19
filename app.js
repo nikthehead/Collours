2c9597a707da0c7ab0558719559b2b78540c29ea
nikthehead <pearw0w@yandex.ru>
2023-01-19 20:13:20 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', event => {
+    event.preventDefault()
+    if (event.code.toLowerCase() == 'space'){
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', event =>{
+    const type = event.target.dataset.type
+
+    if (type == 'lock'){
+        const node = event.target.tagName.toLowerCase() == 'i'
+        ? event.target
+        : event.target.children[0]
+
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    }
+})
+
+function gerenerateRandomColor(){
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for(let i = 0; i < 6; i++){
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+return '#' + color
+}
+
+function setRandomColors(){
+    cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const color = gerenerateRandomColor()
+        const button = col.querySelector('button')
+
+        if (isLocked) {
+            return
+        }
+        
+        text.textContent = color
+        col.style.background = gerenerateRandomColor()
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors()
